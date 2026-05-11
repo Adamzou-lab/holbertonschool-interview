a5a50859b3a1b4060ec3bb3bf4d385274a233b32
Adamzou-lab <adamzpro@icloud.com>
2026-05-11 11:54:06 +0200

    docs: update docstring for pascal_triangle function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Returns a list of lists representing Pascal's triangle of n"""
+"""Module for Pascal's triangle"""
 
 
 def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of n"""
     triangle = [[1]]
     if n <= 0:
         return []
