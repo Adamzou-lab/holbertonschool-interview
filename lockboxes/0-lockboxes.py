b698b3747f27ac82941d9bc6c74c1d309aed2e2e
Adamzou-lab <adamzpro@icloud.com>
2026-05-19 09:22:40 +0200

    feat: add solution for lockboxes problem using depth-first search

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that solves the lockboxes problem using depth-first search"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all locked boxes can be opened"""
+    keys = [0]
+    opened = set()
+
+    while keys:
+        key = keys.pop()
+        opened.add(key)
+
+        if key < len(boxes):
+            for k in boxes[key]:
+                if k not in opened:
+                    keys.append(k)
+
+    return len(opened) == len(boxes)
